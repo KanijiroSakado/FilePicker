7fff8c4468d37ef685d242919382346774287995
Kanijiro Sakado <47074373+KanijiroSakado@users.noreply.github.com>
2026-01-02 16:37:43 +0900

    Add files via upload

---
+++ Example.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sakado
+{
+    internal class Example
+    {
+        public static void Main()
+        {
+            FilePicker filePicker = new FilePicker();
+            filePicker.Add(@"D:\workspace\test1", 10);
+            filePicker.Add(@"D:\workspace\test2", 80);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(filePicker.GetNextFile());
+            }
+
+        }
+    }
+}
